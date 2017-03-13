ab52191cc0fd564d9c4443c9ba14cb8c204aa5db
mnwong <mnwong@umich.edu>
2017-03-12 23:56:53 -0400

    Delete is functional, made score buttons bigger, mobile improvements, added flair selection

--- src/lifecounter.js
+++ src/lifecounter.js
@@ -11,7 +11,9 @@ class SettingsModal extends Component {
 		this.state = {
 			id: props.player.id,
 			name: props.player.name,
-			hp: props.player.hp
+			hp: props.player.hp,
+			playerFlair: props.player.flair,
+			flair: props.flair
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -31,12 +33,13 @@ class SettingsModal extends Component {
 
 	handleSubmit(event){
 		event.preventDefault();
-		this.props.updatePlayer(this.state.id,this.state.name,this.state.hp);
+		this.props.updatePlayer(this.state.id,this.state.name,this.state.hp,this.state.playerFlair);
 		this.props.closeSettingsModal();
 	}
 
 	handleRemove() {
 		this.props.removePlayer(this.state.id);
+		this.props.closeSettingsModal();
 	}
 
 	render() {
@@ -47,7 +50,7 @@ class SettingsModal extends Component {
 					<div className="form-group">
 			      <label htmlFor="name" className="col-lg-3 control-label">Name</label>
 			      <div className="col-lg-9">
-			      	<input type="text" className="form-control" name="name" onChange={this.handleChange} value={this.state.name} />
+			      	<input type="text" className="form-control" name="name" onChange={this.handleChange} value={this.state.name} autoFocus />
 			      </div>
 			    </div>
 
@@ -59,9 +62,22 @@ class SettingsModal extends Component {
 			    </div>
 
 			    <div className="form-group">
+			      <label htmlFor="playerFlair" className="col-lg-3 control-label">Art</label>
+			      <div className="col-lg-9">
+							<select name="playerFlair" className="form-control" onChange={this.handleChange} value={this.state.playerFlair}> 
+								<option value="flair-plains">Plains</option>
+					      <option value="flair-mountain">Mountain</option>
+					      <option value="flair-forest">Forest</option>
+					      <option value="flair-swamp">Swamp</option>
+					      <option value="flair-island">Island</option>
+				      </select>
+			      </div>
+			    </div>
+
+			    <div className="form-group">
 			      <div className="col-lg-9 col-lg-offset-3">
-			      	<input className="btn btn-default col-lg-5" type="submit" value="Update" />
-			      	<input className="btn btn-danger disabled col-lg-5 col-lg-offset-1" onClick={this.handleRemove} value="Delete" />
+			      	<input className="btn btn-default col-lg-5 col-xs-5" type="submit" defaultValue="Update" />
+			      	<input className="btn btn-danger col-lg-5 col-xs-5 col-lg-offset-1 col-xs-offset-1" onClick={this.handleRemove} defaultValue="Delete" />
 			      </div>
 			    </div>
 
@@ -115,7 +131,7 @@ class AddPlayerModal extends Component {
 					<div className="form-group">
 			      <label htmlFor="name" className="col-lg-3 control-label">Name</label>
 			      <div className="col-lg-9">
-			      	<input type="text" className="form-control" name="name" onChange={this.handleChange} value={this.state.name} />
+			      	<input type="text" className="form-control" name="name" onChange={this.handleChange} value={this.state.name} autoFocus />
 			      </div>
 			    </div>
 
@@ -128,7 +144,7 @@ class AddPlayerModal extends Component {
 
 			    <div className="form-group">
 			      <div className="col-lg-9 col-lg-offset-3">
-			      	<input className="btn btn-default" type="submit" value="Add" />
+			      	<input className="btn btn-default" type="submit" defaultValue="Add" />
 			      </div>
 			    </div>
 
@@ -173,7 +189,7 @@ class PlayerContainer extends Component {
 		const newHp = parseInt(this.state.hp) + parseInt(change);
 		this.isAlive(newHp);
 		this.setState({hp: newHp});
-		this.props.updatePlayer(this.props.player.id, this.props.player.name, newHp);
+		this.props.updatePlayer(this.props.player.id, this.props.player.name, newHp, this.props.player.flair);
 	}
 
 	isAlive(hp) {
@@ -195,10 +211,12 @@ class PlayerContainer extends Component {
 					</div>
 				  <div className="panel-body">
 						<h4>{this.state.hp}</h4>
-						<button className="btn btn-default" onClick={this.updateScore} value="-5">-5</button>
-						<button className="btn btn-default" onClick={this.updateScore} value="-1">-1</button>
-						<button className="btn btn-default" onClick={this.updateScore} value="1">+1</button>
-						<button className="btn btn-default" onClick={this.updateScore} value="5">+5</button>
+						<div className="btn-group btn-group-lg" role="group">
+							<button className="btn btn-default" onClick={this.updateScore} value="-5">-5</button>
+							<button className="btn btn-default" onClick={this.updateScore} value="-1">-1</button>
+							<button className="btn btn-default" onClick={this.updateScore} value="1">+1</button>
+							<button className="btn btn-default" onClick={this.updateScore} value="5">+5</button>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -290,23 +308,23 @@ class Lifecounter extends Component {
 				alive: true,
 				flair: this.state.flair[this.randomNumber()]})
 		});
+
 	}
 
 	removePlayer(id) {
-		const newState = this.state.players.map((player,i) => {
-			if(player.id === id) {
-				return update(this.state, {players: {$splice: [[i,1]]}});
-			}
-		});
+		var index = this.state.players.map(function(player){return player.id}).indexOf(id);
+		const newState = this.state.players;
+		newState.splice(index,1);
 		this.setState({players:newState});
 	}
 
-	updatePlayer(id,name,hp) {
+	updatePlayer(id,name,hp,flair) {
 		const newState = this.state.players.map((player) => {
 			if(player.id===id) {
 				const newData = update(player, {
 					name: {$set: name},
-					hp: {$set: hp}
+					hp: {$set: hp},
+					flair: {$set: flair}
 				});
 				return newData;
 			}
@@ -344,7 +362,8 @@ class Lifecounter extends Component {
 		        {this.state.players.map((player) => 
 		        	<PlayerContainer player={player}
 			        	settingsOpen={that.openSettingsModal}
-			        	updatePlayer={this.updatePlayer} />
+			        	updatePlayer={this.updatePlayer}
+			        	key={player.id} />
 		        )}
 		       </div>       
         </div>
@@ -367,7 +386,8 @@ class Lifecounter extends Component {
 	      	<SettingsModal player={this.state.currentPlayer}
 	      		updatePlayer={this.updatePlayer}
 	      		removePlayer={this.removePlayer}
-	      		closeSettingsModal={this.closeSettingsModal} />
+	      		closeSettingsModal={this.closeSettingsModal}
+	      		flair={this.state.flair} />
 	      </ReactModal>
       </div>
     );
